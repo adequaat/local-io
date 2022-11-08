5e486d4fd7ad2708810d9dd69c6b83fec28c4082
Joël Kuijten <joel@pm5544.eu>
2022-11-08 09:25:48 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import { appendFile, readFile, writeFile, rm, mkdir } from "node:fs/promises";
+import { join } from "node:path";
+
+function createPathString(path) {
+  return Array.isArray(path) ? join.apply(null, path) : path;
+}
+
+const encoding = "utf8";
+function normalizeOptions(options) {
+  if (typeof options === "object" && !options.encoding) {
+    options.encoding = encoding;
+  }
+  return options;
+}
+
+export function append(path, content, options = encoding) {
+  return appendFile(createPathString(path), content, normalizeOptions(options));
+}
+
+export function read(path) {
+  return readFile(createPathString(path), "utf8");
+}
+
+export function write(path, content, options = encoding) {
+  return writeFile(createPathString(path), content, normalizeOptions(options));
+}
+
+export function remove(path) {
+  return rm(createPathString(path), { force: true });
+}
+
+export function makeDir(path) {
+  return mkdir(createPathString(path), { recursive: true });
+}
